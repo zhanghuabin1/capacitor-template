1abfe45228a786909629bd4d6fb3d3c34cdc5548
zhbhun <zhbhun@gmail.com>
2024-02-28 15:59:16 +0800

    chore: 初始化

---
+++ android/app/src/main/java/com/pixocial/snapid/miniapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pixocial.snapid.miniapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ dist/assets/web.3de7adea.js
@@ -0,0 +1,10 @@
+import { W as WebPlugin } from "./index.006e4c83.js";
+class SplashScreenWeb extends WebPlugin {
+  async show(_options) {
+    return void 0;
+  }
+  async hide(_options) {
+    return void 0;
+  }
+}
+export { SplashScreenWeb };
